af2c83c9885274dc5052a0e01afe646d4c3c5042
Stefan Vizzari <svizzari@zendesk.com>
2017-05-18 16:28:43 +0100

    refactor(v2): update js for v2

--- src/javascripts/ticket_sidebar.js
+++ src/javascripts/ticket_sidebar.js
@@ -1,5 +1,4 @@
 import View from 'view';
-import Storage from 'storage'
 
 const MAX_HEIGHT = 375;
 
@@ -7,25 +6,34 @@ class TicketSidebar {
   constructor(client, data) {
     this.client = client;
     this._metadata = data.metadata;
-    this._context = data.context;
 
-    this.storage = new Storage(this._metadata.installationId);
-    this.view = new View({ afterRender: () => {
-      let newHeight = Math.min($('html').height(), MAX_HEIGHT);
-      this.client.invoke('resize', { height: newHeight, width: '100%' });
-    }});
-
-    this.getCurrentUser().then(this.renderMain.bind(this));
-
+    this.view = new View({
+      afterRender: () => {
+        let newHeight = Math.min($('html').height(), MAX_HEIGHT);
+        this.client.invoke('resize', {
+          height: newHeight,
+          width: '100%'
+        });
+      }
+    });
+    
     this.view.switchTo('loading');
+    this.init();
   }
 
-  getCurrentUser() {
-    return this.client.request({ url: '/api/v2/users/me.json' });
-  }
-
-  renderMain(data) {
-    this.view.switchTo('main', data.user);
+  async init() {
+    const getData = await this.client.get('ticket.requester.id');
+    const requestData = await this.client.request({
+      url: `/api/v2/users/${getData['ticket.requester.id']}/tickets/requested.json?sort_order=desc`,
+      dataType: 'json'
+    });
+    const sortedTickets = _.chain(requestData.tickets)
+      .sortBy('id')
+      .take(this._metadata.settings.max_tickets_to_display)
+      .value();
+    this.view.switchTo('main', {
+      latestTickets: sortedTickets
+    });
   }
 }
 
